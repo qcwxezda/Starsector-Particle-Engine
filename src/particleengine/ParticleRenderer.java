42333c869e42ce9184b205df921e2fe0e4d6db5f
qcwxezda <hy_pi_corism@live.com>
2025-06-01 23:04:13 -0400

    0.9.2 patch

--- src/particleengine/ParticleRenderer.java
+++ src/particleengine/ParticleRenderer.java
@@ -12,12 +12,13 @@ record ParticleRenderer(Object layer, ParticleAllocator allocator, Particles own
         GL14.glBlendEquation(type.blendMode());
 
         float spriteCenterX = 0.5f, spriteCenterY = 0.5f;
-        if (type.sprite() != null) {
+        boolean hasTexture = type.sprite() != null && type.sprite().getTextureId() > 0;
+        if (hasTexture) {
             int target = 1;
             GL13.glActiveTexture(GL13.GL_TEXTURE0 + target);
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, type.sprite().getTextureId());
             GL20.glUniform1i(ParticleShader.texSamplerLoc, target);
-            GL20.glUniform2f(ParticleShader.textureSizeLoc, type.sprite().getTextureWidth(), type.sprite().getTextureHeight());
+            GL20.glUniform2f(ParticleShader.textureSizeLoc, type.sprite().getTexWidth(), type.sprite().getTexHeight());
             if (type.sprite().getCenterX() >= 0f) {
                 spriteCenterX = type.sprite().getCenterX() / type.sprite().getWidth();
             }
@@ -30,14 +31,14 @@ record ParticleRenderer(Object layer, ParticleAllocator allocator, Particles own
         GL20.glUniform2f(ParticleShader.spriteCenterLoc, spriteCenterX, spriteCenterY);
         GL20.glUniform1f(ParticleShader.viewportAlphaLoc, viewport.getAlphaMult());
         GL30.glBindVertexArray(allocator.vao);
-        GL20.glUniform1i(ParticleShader.useTextureLoc, type.sprite() == null ? 0 : 1);
+        GL20.glUniform1i(ParticleShader.useTextureLoc, !hasTexture ? 0 : 1);
         GL31.glDrawArraysInstanced(
                 GL11.GL_TRIANGLE_STRIP,
                 0,
                 4,
                 allocator.bufferPosition / Particles.FLOATS_PER_PARTICLE);
         GL30.glBindVertexArray(0);
-        if (type.sprite() != null) {
+        if (hasTexture) {
             GL13.glActiveTexture(GL13.GL_TEXTURE0);
         }
     }
